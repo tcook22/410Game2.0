01a5bd329c93dd78a63adffe0e6f40e6cb210d60
CarlosF36 <104406643+CarlosF36@users.noreply.github.com>
2022-06-02 06:51:14 -0700

    Add files via upload

--- 410Game PoC/Assets/Scripts/PlayerCollision.cs
+++ 410Game PoC/Assets/Scripts/PlayerCollision.cs
@@ -10,6 +10,7 @@ public class PlayerCollision : MonoBehaviour
     public GameObject player;
     public GameObject Angel;
     public GameObject gameoverscreen;
+    public GameObject Projectile;
    
 
     private void OnCollisionEnter(Collision other)
@@ -29,6 +30,23 @@ public class PlayerCollision : MonoBehaviour
         if(other.transform.tag == "Angel"){
             Destroy(other.gameObject);
         }
+        if(other.transform.tag == "Projectile"){
+            Destroy(other.gameObject);
+        }
+        if(other.transform.tag == "Shield"){
+            Destroy(other.gameObject);
+        }
+        if(other.transform.tag == "Transparent"){
+            Destroy(other.gameObject);
+        }
+        if(other.transform.tag == "Rocks")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            
+            //gameoverscreen.SetActive(true);
+        
+        }
+            
     }
 
     public void ReplayLevel()
